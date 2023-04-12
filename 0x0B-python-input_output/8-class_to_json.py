23418bad740c08e6aefc4d97191089639f8e538f
festusmaithyakcau <festusmaithya264@gmail.com>
2023-04-12 08:36:50 -0700

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:
    Prototype: def class_to_json(obj):
    obj is an instance of a Class
    All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 8-class_to_json.py
+# Festus Maithya
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
