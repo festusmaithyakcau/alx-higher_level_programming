630b6a8294d4b90b139e0a03aede421e702d3610
festusmaithyakcau <festusmaithya264@gmail.com>
2023-04-12 08:11:21 -0700

    function that writes a string to a text file (UTF8) and returns the number of characters written:
    Prototype: def write_file(filename=, text=):
    You must use the with statement
    You don’t need to manage file permission exceptions.
    Your function should create the file if doesn’t exist.
    Your function should overwrite the content of the file if it already exists.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-# 1-number_of_lines.py
+# 1-write_file.py
 # Festus Maithya
-"""Defines a text file line-counting function."""
+"""Defines a file-writing function."""
 
 
-def number_of_lines(filename=""):
-    """Return the number of lines in a text file."""
-    lines = 0
-    with open(filename) as f:
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
